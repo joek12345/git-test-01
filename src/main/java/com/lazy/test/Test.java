98b6a0688f485c86e6d5c949a33bc360ab745e25
JoeLazy <321136153@qq.com>
2023-05-12 21:18:59 +0800

    第一次提交

---
+++ src/main/java/com/lazy/test/Test.java
@@ -0,0 +1,12 @@
+package com.lazy.test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Joe_Lazy
+ * Date: 2023/5/12 21:18:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello China");
+    }
+}
