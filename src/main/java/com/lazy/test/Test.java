257df08a961e60e823fc6e73f476fbc362dc6d4e
JoeLazy <321136153@qq.com>
2023-05-12 21:19:43 +0800

    第三次提交

--- src/main/java/com/lazy/test/Test.java
+++ src/main/java/com/lazy/test/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello China");
         System.out.println("hello ShanDong");
+        System.out.println("hello JiNan");
     }
 }
